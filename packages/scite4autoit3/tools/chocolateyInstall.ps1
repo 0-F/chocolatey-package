c9edf6043f0763837743f23cd6e225bb935ed48c
0-F <dddd@gmx.fr>
2015-02-02 16:31:40 +0100

    (scite4autoit3)

---
+++ packages/scite4autoit3/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'scite4autoit3'
+$installerType = 'exe'
+$url = 'http://www.autoitscript.com/autoit3/scite/download/SciTE4AutoIt3.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
