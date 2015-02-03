d38ec13de0e0de3f2c6bfef89e5c09456505bc07
0-F <dddd@gmx.fr>
2015-02-03 01:07:55 +0100

    (scite4autoit3) transfert vers "automatic"

--- packages/scite4autoit3/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$packageName = 'scite4autoit3'
-$installerType = 'exe'
-$url = 'http://www.autoitscript.com/autoit3/scite/download/SciTE4AutoIt3.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
