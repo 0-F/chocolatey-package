4ff22999e7e78f12f0155b1cf16f72f8aa020237
0-F <dddd@gmx.fr>
2015-02-12 19:02:27 +0100

    (virustotaluploader)

---
+++ automatic/virustotaluploader/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$url = '{{DownloadUrl}}'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
